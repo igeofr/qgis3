f2cc78ca75612bb872be918d908c59d8d797996d
Flø <florian.boret@data-wax.com>
2020-09-14 22:59:47 +0200

    Create Rules_style_to_mapfile_algo.py

---
+++ scripts/Rules_style_to_mapfile_algo.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+#import csv
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterVectorLayer,
+                       QgsProcessingParameterFileDestination,
+                       QgsProcessingParameterBoolean)
+from qgis import processing
+
+
+class ExampleProcessingAlgorithm(QgsProcessingAlgorithm):
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    BOOLEAN1 = 'BOOLEAN1'
+    OUTPUT = 'OUTPUT'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return ExampleProcessingAlgorithm()
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'Rules style to Mapfile'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('Rules style to Mapfile')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('Mapfile')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'mapfile'
+
+    def shortHelpString(self):
+        """
+        Returns a localised short helper string for the algorithm. This string
+        should provide a basic description about what the algorithm does and the
+        parameters and outputs associated with it..
+        """
+        return self.tr("Permet d'exporter un style QGIS de règles en Mapfile")
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+        self.addParameter(
+            QgsProcessingParameterVectorLayer(
+                self.INPUT,
+                self.tr('Fichier vectoriel'),
+                [QgsProcessing.TypeVectorAnyGeometry]
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterBoolean(
+                self.BOOLEAN1,
+                self.tr('Interrogation WMS'),
+                defaultValue=False
+            )
+        )
+        self.addParameter(
+            QgsProcessingParameterFileDestination(
+                self.OUTPUT,
+                self.tr('Output layer'),
+                'TXT files (*.txt)'
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+        source = self.parameterAsVectorLayer(parameters, self.INPUT, context)
+        txt_file = self.parameterAsFileOutput(parameters, self.OUTPUT, context)
+        boolean_value = self.parameterAsBool(parameters, self.BOOLEAN1, context)
+
+        renderer  = source.renderer()
+
+        # Open a file with access mode 'w'
+        file_object = open(txt_file, 'w')
+
+
+        if renderer.type() == 'RuleRenderer': #Style categorise
+            root_rule = renderer.rootRule().children()
+
+            for rule in root_rule: # Recuperation des infos et ecriture dans le txt
+                line1 = 'CLASS'
+                line2 = '   NAME "'+(rule.label())+'"'
+                if boolean_value==True :
+                    line3 = '       TEMPLATE "../template/getfeatureinfo/{LAYER_NAME}.html"'
+                else :
+                    line3 =''
+                line4 = '       EXPRESSION ("[niv3]" eq "'+rule.filterExpression ()+'")'
+                line5 = '       STYLE'
+                line6 = '           COLOR '+str(rule.symbol().color().red())+' '+str(rule.symbol().color().green())+' '+str(rule.symbol().color().blue())
+                line7 = '           '#OUTLINECOLOR '+str(rule.symbol().symbolLayer(0).strokeColor().red())+' '+str(rule.symbol().symbolLayer(0).strokeColor().green())+' '+str(rule.symbol().symbolLayer(0).strokeColor().blue())
+                line8 = '           '#WIDTH '+str(rule.symbol().symbolLayer(0).strokeWidth())
+                line9 = '       END'
+                line10 = '  END'
+                # Append 'hello' at the end of file
+                file_object.write('{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n'.format(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10))
+                # Close the file
+        file_object.close()
+        # Return the results of the algorithm. In this case our only result is
+        # the feature sink which contains the processed features, but some
+        # algorithms may return multiple feature sinks, calculated numeric
+        # statistics, etc. These should all be included in the returned
+        # dictionary, with keys matching the feature corresponding parameter
+        # or output names.
+        return {self.OUTPUT: txt_file}
